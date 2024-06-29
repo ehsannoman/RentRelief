23a33a73e2dde03240f7f6407859601352ce33f9
Noman Ehsan <119247974+ehsannoman@users.noreply.github.com>
2024-06-29 23:21:34 +0600

    main.php

--- main.php
+++ main.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+include 'config.php';
+
+$query = "SELECT * FROM posts";
+$result = $conn->query($query);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,43 +15,42 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <div class="blur-bg"></div> <!-- Div for blurred background -->
+    <div class="blur-bg"></div> 
     
     <div class="content">
-        <!-- Your existing content here -->
-        <!-- Navbar -->
+        
         <div class="navbar">
             <img src="logo.png" alt="Logo" class="logo">
             <div class="navbar-icons">
+                <a href="user.php"><img src="user.png" alt="Profile" class="icon"></a>
                 <img src="notification.png" alt="Notifications" class="icon">
-                <img src="profile.png" alt="Profile" class="icon">
             </div>
         </div>
 
-        <!-- Sidebar -->
         <div class="sidebar">
             <ul>
                 <li><a href="#">Posts</a></li>
-                <li><a href="#">Create Post</a></li>
+                <li><a href="createpost.php">Create Post</a></li>
                 <li><a href="#">My Posts</a></li>
                 <li><a href="#">Wishlist</a></li>
-                <li><a href="#">Logout</a></li>
+                <li><a href="home.html">Logout</a></li>
             </ul>
         </div>
 
-        <!-- Main Content -->
         <div class="main-content">
-            <!-- Example post card -->
+            <?php while ($row = $result->fetch_assoc()): ?>
             <div class="post-card">
-                <img src="image1.jpg" alt="Post Image" class="post-image">
+                <img src="<?php echo htmlspecialchars(explode(', ', $row['images'])[0]); ?>" alt="Post Image" class="post-image">
                 <div class="post-details">
-                    <p>Location: Mohanagar</p>
-                    <p>Rent Type: Single Room</p>
-                    <button class="details-button">Details</button>
+                    <p>Location: <?php echo htmlspecialchars($row['area']); ?></p>
+                    <p>Rent Type: <?php echo htmlspecialchars($row['type']); ?></p>
+                    <p>Price: <?php echo htmlspecialchars($row['price']); ?></p>
+                    <button class="details-button" onclick="window.location.href='details.php?id=<?php echo $row['id']; ?>'">Details</button>
                     <img src="wishlist.png" alt="Add to Wishlist" class="wishlist-icon">
                 </div>
             </div>
-            <!-- Repeat the post-card div for more posts -->
+            <?php endwhile; ?>
+            <?php $conn->close(); ?>
         </div>
     </div>
 </body>
